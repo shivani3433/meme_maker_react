ae176de725cccfcdec8377a881f45ae3efdae6fc
Shivani Sharma <shivanisharma@Shivanis-MacBook-Air.local>
2021-09-15 13:47:42 +0530

    test branch

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Button(props) {
   return (
     <div className="mt-3 text-center">
-      <button className="btn btn-dark mr-5 test-commit" onClick={props.click}>
+      <button className="btn btn-dark mr-5 test-commit-branch" onClick={props.click}>
         {props.name}
       </button>
     </div>
